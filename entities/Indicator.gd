127ee2f2d91ee0ab3464297bd2484359a815865a
Federico Frenguelli <synasius@gmail.com>
2019-03-20 23:40:42 +0100

    Refactored Indicator movement

--- entities/Indicator.gd
+++ entities/Indicator.gd
@@ -1,29 +1,87 @@
 extends Node2D
+class_name Grid
 
-var direction = Vector2(0, 0)
+# The movement speed of the indicator
+export var speed: float = 20.0
+
+const PAUSE_DURATION_SLOW = 0.15
+const PAUSE_DURATION_FAST = 0.03
+
+# Represents how long the cursor wait before
+# it can move again
+var _pause_duration: float = PAUSE_DURATION_SLOW
+
+var direction: Vector2 = Vector2.ZERO
+
+# This is used to decrease change pause duration
+# to implement fast moving when player moves only
+# in one direction
+var _last_direction: Vector2 = Vector2.ZERO
+
+# Stores where the object was when the movement started
+# This member variable is needed to interpolate movement
+var _start_position: Vector2 = Vector2.ZERO
+# Stores where the movement should end
+var _target_position: Vector2 = Vector2.ZERO
+
 var type = "indicator"
 var player = null
 
-onready var grid = get_parent()
+onready var grid: Grid = get_parent()
 
-func _process(delta):
-	var is_moving = Input.is_action_pressed("ui_down") or Input.is_action_pressed("ui_right") \
-				or Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_up")
-				
-	if is_moving:
-		set_process(false)
-		if Input.is_action_pressed("ui_down"):
-			direction += Vector2(0, 1)
-		if Input.is_action_pressed("ui_up"):
-			direction += Vector2(0, -1)
-		if Input.is_action_pressed("ui_left"):
-			direction += Vector2(-1, 0)
-		if Input.is_action_pressed("ui_right"):
-			direction += Vector2(1, 0)
-		
-		$timer.start()
-		grid.update_child_position(position, direction, type, player)
-		direction = Vector2.ZERO
+enum States {
+	IDLE,
+	MOVING,
+	PAUSE,
+}
 
-func _on_timer_timeout():
-	set_process(true)
+var _state = States.IDLE
+var _elapsed = 0
+
+static func get_input_direction() -> Vector2:
+	return Vector2(
+		float(Input.is_action_pressed("move_right")) - float(Input.is_action_pressed("move_left")),
+		float(Input.is_action_pressed("move_down")) - float(Input.is_action_pressed("move_up"))
+	)
+
+func _process(delta: float) -> void:
+	if _state == States.IDLE:
+		process_idle()
+	elif _state == States.MOVING:
+		process_moving(delta)
+	elif _state == States.PAUSE:
+		process_pause(delta)
+
+func process_idle() -> void:
+	direction = get_input_direction()
+
+	# if the user is moving in the same direction again decrease
+	# the pause duration to allow fast movement
+	if direction == Vector2.ZERO or _last_direction != direction:
+		_pause_duration = PAUSE_DURATION_SLOW
+	else:
+		_pause_duration = PAUSE_DURATION_FAST
+
+	# stores that last direction which will be use on the
+	# next frame to increase/decrease pause duration
+	_last_direction = direction
+
+	if direction != Vector2.ZERO:
+		if grid.can_move(self, direction):
+			_target_position = grid.update_child_position(self)
+			_start_position = position
+			_state = States.MOVING
+
+func process_moving(delta: float) -> void:
+	_elapsed += delta
+	var weight = _elapsed * speed
+	position = _start_position.linear_interpolate(_target_position, min(weight, 1))
+	if weight >= 1:
+		_elapsed = 0
+		_state = States.PAUSE
+
+func process_pause(delta: float) -> void:
+	_elapsed += delta
+	if _elapsed >= _pause_duration:
+		_elapsed = 0
+		_state = States.IDLE
--- scenes/Grid.gd
+++ scenes/Grid.gd
@@ -1,11 +1,12 @@
 extends TileMap
+class_name Player
 
-var tile_size = get_cell_size()
-var half_tile_size = self.tile_size / 2
+var tile_size : Vector2 = get_cell_size()
+var half_tile_size : Vector2 = self.tile_size / 2
 
-# here we could compute the grid size using 
+# here we could compute the grid size using
 # the viewport size divided by the cell size
-onready var grid_size = Vector2(960 / tile_size.x, 
+onready var grid_size = Vector2(960 / tile_size.x,
 								512 / tile_size.y)
 var grid = []
 
@@ -19,15 +20,15 @@ func _ready():
 		grid.append([])
 		for y in range(grid_size.y):
 			grid[x].append({})
-	
+
 	# init first player
 	var player1 = Player.instance()
 	player1.position = map_to_world(Vector2(1, 1)) + self.half_tile_size
 	player1.top_left = Vector2.ZERO
-	player1.bottom_right = Vector2(floor(grid_size.x / 2), grid_size.y) 
+	player1.bottom_right = Vector2(floor(grid_size.x / 2), grid_size.y)
 	grid[1][1][player1.type] = player1
 	add_child(player1)
-	
+
 	# init second player
 	var player2 = Player.instance()
 	player2.position = map_to_world(Vector2(grid_size.x - 2, grid_size.y - 2)) + self.half_tile_size
@@ -35,7 +36,7 @@ func _ready():
 	player2.bottom_right = grid_size
 	grid[grid_size.x - 2][grid_size.y - 2][player2.type] = player2
 	add_child(player2)
-	
+
 	# indicator instance
 	var indicator = Indicator.instance()
 	indicator.position = player1.position
@@ -43,27 +44,27 @@ func _ready():
 	grid[1][1][indicator.type] = indicator
 	add_child(indicator)
 
-func is_valid_position(player, grid_position):
+func can_move(child_node, direction) -> bool:
+	var grid_position: Vector2 = world_to_map(child_node.position) + direction
+	var player: Player = child_node.player
+
 	if grid_position.x < player.bottom_right.x and grid_position.x >= player.top_left.x:
 		if grid_position.y < player.bottom_right.y and grid_position.y >= player.top_left.y:
 			return true
 	return false
 
-func update_child_position(position, direction, type, player):
-	var grid_pos = world_to_map(position)
-	var new_pos = grid_pos + direction
-	# here we check that the current position is not on the boundaries
-	# if it's not we can move :D
-	if is_valid_position(player, new_pos):
-		# get the entity on the grid of the same type
-		# of the object that wants to update its position
-		var entity = grid[grid_pos.x][grid_pos.y][type]
-		
-		# move the item on the grid
-		grid[grid_pos.x][grid_pos.y].erase(type)
-		grid[new_pos.x][new_pos.y][type] = entity
-		# move the item on the physical world 
-		# TODO: this part could be handled by the item directly 
-		#       returning the new pos
-		entity.position = map_to_world(new_pos) + half_tile_size
-		
+func update_child_position(child_node) -> Vector2:
+	var grid_pos = world_to_map(child_node.position)
+	var new_pos = grid_pos + child_node.direction
+
+	# get the entity on the grid of the same type
+	# of the object that wants to update its position
+	var entity = grid[grid_pos.x][grid_pos.y][child_node.type]
+
+	# move the item on the grid
+	grid[grid_pos.x][grid_pos.y].erase(child_node.type)
+	grid[new_pos.x][new_pos.y][child_node.type] = entity
+	# move the item on the physical world
+	# TODO: this part could be handled by the item directly
+	#       returning the new pos
+	return map_to_world(new_pos) + half_tile_size
