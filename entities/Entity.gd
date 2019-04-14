d0a503dc7b352aafc3d0db47aea61878cf1b5572
Federico Frenguelli <synasius@gmail.com>
2019-04-14 07:46:56 +0200

    Moved default implementation of the action method from Planet to Entity

--- entities/Entity.gd
+++ entities/Entity.gd
@@ -5,3 +5,10 @@ var type = GridObjectType.INVALID
 
 var player: Player = null
 var health: int = 100
+
+func action() -> void:
+	# This method is needed because the game
+	# calls `action` method on the entity that
+	# is under the indicator. The default impl
+	# is empty at the moment
+	pass
--- entities/Planet.gd
+++ entities/Planet.gd
@@ -5,10 +5,3 @@ export var resource_quantity = 100;
 
 func _ready() -> void:
 	self.type = GridObjectType.PLANET
-
-func action() -> void:
-	# This method is needed because the game
-	# calls `action` method on the entity that
-	# is under the indicator. The default impl
-	# is empty at the moment
-	pass
